0e7e899e3886639fc8a69ea8beb05e79ed54c0a0
Biruk Worku <bworku1221@gmail.com>
2025-08-28 18:48:29 +0530

    Update AiTools.jsx
    trying to fixx that css issue

--- client-side/src/components/AiTools.jsx
+++ client-side/src/components/AiTools.jsx
@@ -23,7 +23,7 @@ const AiTools = () => {
                     <h3 className='mt-6 mb-3 text-lg font-semibold'>
                         {tool.title}
                     </h3>
-                    <p className='text-gray-400 text-sm max-w-[95]'>
+                    <p className='text-gray-400 text-sm max-w-[95px]'>
                         {tool.description}
                     </p>
                    
